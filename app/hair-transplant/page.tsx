4d2c4f7c5cafb0b6ab0a27246486cdad5c65e577
Anonymous-hss <harshalsawatkar24@gmail.com>
2025-03-13 12:06:45 +0530

    First commit

--- app/hair-transplant/page.tsx
+++ app/hair-transplant/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import CarbonFacial from "./carbon-faical/page";
 
 function page() {
-  CarbonFacial();
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hair Transplant</h1>
     </div>
   );
 }
